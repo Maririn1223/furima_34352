d2eb4f2d6fc3e2158faaef20bc10bc799fc3e4e0
Maririn1223 <79556323+Maririn1223@users.noreply.github.com>
2021-06-06 13:27:48 +0900

    アソシエーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,13 @@
 class Product < ApplicationRecord
+  belongs_to :user
+  belongs_to_active_hash :tags  
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :charge
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :order_date
+  has_one                :purchase
+  has_one                :delivery
+
   validates :title,         presence: true
   validates :text,          presence: true
   validates :tag_id,        presence: true
