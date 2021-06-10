9c562f7bbf578ad81d37febb6ca176f26aa39ab9
Maririn1223 <79556323+Maririn1223@users.noreply.github.com>
2021-06-10 18:58:31 +0900

    item_spec.rbの編集」

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe Item, type: :model do
   end
 
   describe '商品出品機能' do
-    context '商品の出品がうまくいかない時' do
+    context '商品の出品がうまくいく時' do
       it '商品画像、商品名、商品説明・状態、カテゴリーの情報、配送料、発送元、発送までの日数、販売価格があれば登録できる' do
         expect(@item).to be_valid
       end
     end
 
-    context '商品の出品がいまくいかない時' do
+    context '商品の出品がうまくいかない時' do
       it '商品画像が空では登録できない' do
         @item.image = nil
         @item.valid?
