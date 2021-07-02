a44a78adb5911342bc2b857774a08c0534bb8efb
Maririn1223 <79556323+Maririn1223@users.noreply.github.com>
2021-07-02 12:53:31 +0900

    ログイン状態、商品が売れているときトップページへ

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,13 +3,21 @@ class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
 
   def index
+    if user_signed_in?
+      if @item.order.present?
+        redirect_to root_path
+      end
+    end   
+    
     @order_address = OrderAddress.new
     redirect_to root_path if current_user.id == @item.user_id
   end
 
   def create
     @order_address = OrderAddress.new(order_params)
-    if @order_address.valid?
+
+
+     if @order_address.valid?
       pay_item
       @order_address.save
       redirect_to root_path
@@ -17,11 +25,7 @@ class OrdersController < ApplicationController
       render :index
     end
 
-    if user_signed_in?
-      if @item.order_present?
-        redirect_to root_path
-      end
-    end
+
 
   end
 
